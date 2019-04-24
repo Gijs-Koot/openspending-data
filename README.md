686cb900231485a0c276d5df7ec345f6430f28e9
Gijs Koot <gijs.koot@axians.com>
2019-04-24 15:10:33 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# openspending-data
+# `openspending-data`
+
 Python wrapper for accessing the OpenSpending API
+
+## Usage
+
+```
+from openspending import OpenSpending
+
+os = OpenSpending()
+
+# retrieve all governments
+govs = os.governments(params={}, max=800) # returns a list
+
+# retrieve all entries from a specific year from a specific gov body
+entries = os.entries(params=dict(year=2016, gov_code=358, max=20000) # returns a list
+```
+
---
+++ openspending.py
@@ -0,0 +1,42 @@
+import requests
+from urllib.parse import urljoin
+from itertools import islice
+import logging
+
+
+class OpenSpending(object):
+
+    base_url = "https://openspending.nl"
+
+    def entries(self, params: dict, max=100) -> list:
+
+        path = "api/v1/entries"
+        return list(islice(self.gen_objects(path, params), max))
+
+    def governments(self, params: dict, max=100) -> list:
+
+        path = "api/v1/governments"
+        return list(islice(self.gen_objects(path, params), max))
+
+    def gen_objects(self, path, params):
+
+        nxt = path
+
+        while nxt:
+
+            url = urljoin(self.base_url, nxt)
+            raw = requests.get(url, params=params)
+
+            try:
+                response = raw.json()
+                for obj in response["objects"]:
+                    yield obj
+                nxt = response["meta"]["next"]
+            except requests.JSONDecodeError as e:
+                logging.warning(f"Got error {e}\n{raw.content}")
+
+
+if __name__ == "__main__":
+
+    os = OpenSpending()
+    print(os.entries(gov_code="GM"))
